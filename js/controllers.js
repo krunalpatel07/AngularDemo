ae5f3f2a3a400b32578a8bb82bcd56fccfed0d9b
Krunal Patel <kp25315@gmail.com>
2016-04-26 23:18:31 -0700

    draft version

---
+++ js/controllers.js
@@ -0,0 +1,29 @@
+var appControllers = angular.module('appControllers',[
+]);
+
+appControllers.controller('driverController',function($scope){
+	$scope.driverList = [
+      {
+          Driver: {
+              givenName: 'Sebastian',
+              familyName: 'Vettel'
+          },
+          points: 322,
+          nationality: "German",
+          Constructors: [
+              {name: "Red Bull"}
+          ]
+      },
+      {
+          Driver: {
+          givenName: 'Fernando',
+              familyName: 'Alonso'
+          },
+          points: 207,
+          nationality: "Spanish",
+          Constructors: [
+              {name: "Ferrari"}
+          ]
+      }
+    ];
+});
